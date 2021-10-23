9d493aa7811c2a8f541d3c4427001ed855fcba58
deilquesce <63884726+deilquesce@users.noreply.github.com>
2021-10-23 10:36:29 -0400

    Update URI

---
+++ db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store",
+    "URI":"mongodb+srv://daniel:WMLgqeIGyXEBbcka@mongodbserver.zgk3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
